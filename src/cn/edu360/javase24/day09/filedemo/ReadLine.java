0131d6342c3c8647e1d1be6ef20c3cd981e734ea
py <237394602@qq.com>
2019-01-16 23:44:32 +0800

    新增BufferedReaderDemo

---
+++ src/cn/edu360/javase24/day09/filedemo/ReadLine.java
@@ -0,0 +1,25 @@
+package cn.edu360.javase24.day09.filedemo;
+
+import java.io.FileInputStream;
+/*
+ * ∞¥––∂¡»°
+ */
+public class ReadLine {
+
+	public static void main(String[] args) throws Exception {
+		FileInputStream fis = new FileInputStream("a.txt");
+		
+		byte[] buf = new byte[1024];
+		int num=0;
+		int i = 0;
+		while ((num=fis.read()) != -1){
+			if(num==13){
+				break;
+			}
+			buf[i]=(byte)num;
+			i++;
+		}
+		System.out.println(new String(buf,0,i)); 
+	}
+
+}
