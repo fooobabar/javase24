3c3074918b69443bd04144ee957d76abf96a19ee
iii <iii@test.com>
2019-01-17 09:16:18 +0800

    解决冲突

--- src/cn/edu360/javase24/day09/filedemo/FileInputStreamDemo.java
+++ src/cn/edu360/javase24/day09/filedemo/FileInputStreamDemo.java
@@ -1,6 +1,9 @@
 package cn.edu360.javase24.day09.filedemo;
 
 import java.io.FileInputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FileInputStreamDemo {
 
@@ -8,18 +11,38 @@ public class FileInputStreamDemo {
 		// TODO Auto-generated method stub
 		FileInputStream fis = new FileInputStream("d:/a.txt");
 		
+		//一个一个读
 		int i;
 		while((i = fis.read())!=-1){
-			System.out.println((char)i);
+			System.out.println(i+" "+(char)i);
 		}
 		
 		fis.close();
 		
-		FileInputStream fis2 = new FileInputStream("d:/b.txt");
+		System.out.println("--------------分隔符--------------");
 		
-		char[] ch = new char[3];
+		
+		FileInputStream fis2 = new FileInputStream("d:/a.txt");
+		
+		byte[] ch = new byte[3];
 		
 		int num = fis2.read(ch);
+		System.out.println(new String(ch,0,2));   // 从0开始，往下偏移2个
+		System.out.println(Arrays.toString(ch));
+		
+		fis2.close();
+		
+		System.out.println("--------------分隔符--------------");
+		FileInputStream fis3 = new FileInputStream("d:/a.txt");
+		int num2 = 0;
+		byte[] byte1=new byte[8];
+		
+		while( (num2 = fis3.read(byte1)) != -1){
+			System.out.println(Arrays.toString(byte1));
+			System.out.println(new String(byte1));
+		}
+		
+		
 		
 	}
 
---
+++ src/cn/edu360/javase24/day09/filedemo/ReadLine.java
@@ -0,0 +1,10 @@
+package cn.edu360.javase24.day09.filedemo;
+
+public class ReadLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
